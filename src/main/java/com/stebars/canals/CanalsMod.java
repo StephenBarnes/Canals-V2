0525d46008452e14624ba2c88d5b0311d20a7de2
Stephen Barnes <stephen6174@gmail.com>
2021-10-20 22:31:55 -0500

    Add compatibility with flowing fluid worldgen

--- src/main/java/com/stebars/canals/CanalsMod.java
+++ src/main/java/com/stebars/canals/CanalsMod.java
@@ -1,7 +1,13 @@
 package com.stebars.canals;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.FlowingFluidBlock;
+import net.minecraft.util.registry.Registry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import virtuoel.statement.api.StateRefresher;
 
 
 @Mod(CanalsMod.MOD_ID)
@@ -11,5 +17,14 @@ public class CanalsMod {
 
 	public CanalsMod() {
 		MinecraftForge.EVENT_BUS.register(this);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::injectFineLevel);
+		StateRefresher.INSTANCE.reorderBlockStates();
+	}
+
+	public void injectFineLevel(FMLCommonSetupEvent e) {
+		for (Block block : Registry.BLOCK) {
+			if (block instanceof FlowingFluidBlock && !((FlowingFluidBlock) block).getFluid().isSource(block.defaultBlockState().getFluidState()))
+				StateRefresher.INSTANCE.addBlockProperty(block, Util.BLOCK_FINE_LEVEL, 11);
+		}
 	}
 }
